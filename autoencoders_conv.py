9b4e4bc4e7350ab52047d4595d9f226f8af0e9f5
Ghiles <ghiles.cherfaoui@soat.fr>
2019-05-24 10:53:26 +0200

    update producer dataset and remove logger conflict

--- autoencoders_conv.py
+++ autoencoders_conv.py
@@ -1,7 +1,3 @@
-# %% md
-# MNIST import
-# %%
-
 import keras
 from keras.callbacks import *
 from keras.datasets import *
@@ -9,16 +5,29 @@ from keras.layers import *
 from keras.metrics import *
 from keras.models import *
 from keras.optimizers import *
+from keras_preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot as plt
 
-experiment_name = "Mnist_Conv2D_auto_Encoders"
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+experiment_name = "Mnist_Conv2D_auto_Encoders_test"
+
+train_data_dir = '/home/soat/ibd_5a_clustering/fruits-360/test_images28.npy'
+nb_train_samples = 17845
+batch_size = 256
+
+(x_train, y_train), (x_test, y_test) = train_data_dir
+x_train = np.load(train_data_dir)
+
+print(x_train.shape)
+exit(0)
+
 y_train = keras.utils.to_categorical(y_train)
 y_test = keras.utils.to_categorical(y_test)
-
+#
 x_train = np.reshape(x_train, (-1, 28, 28, 1)) / 255.0
 x_test = np.reshape(x_test, (-1, 28, 28, 1)) / 255.0
+print(x_train.shape, y_train.shape)
 
 tb_callback = TensorBoard("./logs/" + experiment_name)
 
@@ -46,13 +55,34 @@ decoder = Conv2DTranspose(1, (3, 3), activation='relu', padding='same')(decoder)
 autoencoder = Model(input, decoder)
 encoder = Model(input, encoder)
 
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(28, 28),
+    shuffle=True, seed=None,
+    class_mode='categorical',
+    interpolation='nearest')
+
 autoencoder.compile(Adam(), mse, metrics=[categorical_accuracy])
 
-autoencoder.fit(x_train, x_train,
-                batch_size=128,
-                epochs=5,
-                shuffle=True,
-                callbacks=[tb_callback])
+autoencoder.fit_generator(train_generator, train_generator,
+                          steps_per_epoch=nb_train_samples // batch_size,
+                          epochs=1,
+                          verbose=1,
+                          validation_data=None,
+                          validation_steps=None,
+                          class_weight=None,
+                          max_queue_size=10,
+                          workers=1,
+                          use_multiprocessing=False,
+                          shuffle=True,
+                          initial_epoch=0,
+                          callbacks=[tb_callback])
 
 encoded_imgs = encoder.predict(x_test)
 predicted = autoencoder.predict(x_test)
--- produce_dataset.py
+++ produce_dataset.py
@@ -1,25 +1,27 @@
 import os
+
 import cv2
 import numpy as np
 
-
-path = "C:/Users/alevy/esgi/ml/ibd_5a_clustering/datasets/fruits-360_dataset/fruits-360/"
+path = "/home/soat/ibd_5a_clustering/fruits-360/"
 
 os.chdir(path)
 directory = os.listdir('Test')
 
-liste_image, liste_labels = [], []
+list_image, list_labels = [], []
 
 for idx, classe in enumerate(directory):
     tmp = os.listdir('Test/' + classe)
     for i in tmp:
         img = cv2.imread('Test/' + classe + "/" + i)
-        blank_image = cv2.resize(img,(28, 28))
-        liste_image.append(img)
-        liste_labels.append(idx)
+        image = cv2.resize(img, (28, 28))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-x = np.asarray(liste_image)
-y = np.asarray(liste_labels)
+        list_image.append(image)
+        list_labels.append(idx)
+
+x = np.asarray(list_image)
+y = np.asarray(list_labels)
 
 np.save("test_images28.npy", x)
 np.save("test_labels28.npy", y)

c7617964c45b0778bae5727564a21b20c68483b6
antoine <antoine.levy@iscool-e.com>
2019-05-24 13:54:26 +0200

    add msg_gan

--- msg_gan.py
+++ msg_gan.py
@@ -1,4 +1,46 @@
-from gan.py import *
+import time, os, random
+import tensorflow as tf
+from matplotlib import pyplot as plt
+import numpy as np
+from tensorflow.python.keras import Input, Model
+# from tensorflow.python.keras.utils import plot_model
+from tensorflow.python.keras.layers import Dense, LeakyReLU, BatchNormalization, Concatenate
+from tensorflow.keras.callbacks import TensorBoard
+
+def named_logs(model, logs):
+  result = {}
+  for l in zip(model.metrics_names, [logs]):
+    result[l[0]] = l[1]
+  return result
+
+def load_and_show(gan_folder="gan_1558556204"):
+    directory="logs/" + gan_folder + "/"
+    from os import listdir
+    from os.path import isfile, join
+    files = [f for f in listdir(directory) if isfile(join(directory, f))]
+    print("found " + str(len(files)-1) + " files !")
+    for i in range(len(files)):
+        if files[i] != "info":
+            plt.imshow(np.load(directory + files[i]).reshape(28, 28))
+            plt.gray()
+            plt.show()
+
+def generate_test_images(generator, nb_images, latent_space_dim=2):
+    for i in range(nb_images):
+        plt.imshow(generator.predict(np.random.rand(1, latent_space_dim)).reshape(28, 28))
+        plt.gray()
+        plt.show()
+
+def generate_grid(generator, grid_size, latent_space_dim=2):
+    fig = plt.figure(figsize=(50, 50))
+    columns = 10
+    rows = 10
+    for i in range(1, columns * rows + 1):
+        img = generator.predict(np.array([[0.1*i/columns, 0.1*i%columns]])).reshape(28, 28)
+        fig.add_subplot(rows, columns, i)
+        plt.imshow(img)
+        plt.gray()
+    plt.show()
 
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
@@ -21,86 +63,69 @@ x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
 def create_msg_model(data_shape_1, data_shape_2, data_shape_3, latent_space_dim ):
 
-    input_1 = Input(shape=data_shape_1)
-    discriminator_1 = Dense(data_shape)(input)
-    discriminator_1  = LeakyReLU()(discriminator_1)
-    discriminator_1  = Dense(512)(discriminator_1)
-    discriminator_1  = LeakyReLU()(discriminator_1)
-    discriminator_1  = Dense(256)(discriminator_1)
-    discriminator_1  = LeakyReLU()(discriminator_1)
-    discriminator_1 = Dense(256)(discriminator_1)
-    discriminator_1 = LeakyReLU()(discriminator_1)
-    discriminator_1 = Dense(128)(discriminator_1)
-    discriminator_1 = LeakyReLU()(discriminator_1)
-    discriminator_1 = Dense(64)(discriminator_1)
-    discriminator_1 = LeakyReLU()(discriminator_1)
-    input_2 = Input(shape=data_shape_2)
-    discriminator_2 = Dense(32)(discriminator_1)(input_2)
-    discriminator_2 = LeakyReLU()(discriminator_2)
-    input_3 = Input(shape=data_shape_3)
-    discriminator_3 = Dense(16)(discriminator_2)(input_3)
-    discriminator_3 = LeakyReLU()(discriminator_3)
-    discriminator_3  = Dense(1, activation='sigmoid')(discriminator_3 )
+    input_1 = Input(shape=(784,))
+    input_2 = Input(shape=(64,))
+    input_3 = Input(shape=(16,))
 
-    discriminator_3_model = Model(input_3, discriminator_3)
-    discriminator_3_model.compile(optimizer='adam', loss='mse')
+    disc_1  = Dense(512)
+    disc_2  = Dense(256)
+    disc_3  = Dense(128)
+    disc_4  = Dense(64)
+    disc_5  = Dense(16)
+    disc_6  = Dense(1)
 
-    discriminator_d2 = Model(input_2, discriminator_2)
-    discriminator_2_model = Model(discriminator_d2, discriminator_3)
-    discriminator_2_model.compile(optimizer='adam', loss='mse')
+    disc_tensor_output = \
+        disc_6(disc_5(Concatenate()([input_3,
+            disc_4(Concatenate()([input_2,
+                disc_3(disc_2(disc_1(input_1)))
+                                ]))])))
+    discriminator_model = Model([input_1, input_2, input_3], disc_tensor_output)
+    discriminator_model.compile(optimizer='adam', loss='mse')
 
-    discriminator_d1 = Model(input_1, discriminator_1)
-    discriminator_d2 = Model(discriminator_d1, discriminator_d2)
-    discriminator_1_model = Model(discriminator_d2, discriminator_3)
-    discriminator_1_model.compile(optimizer='adam', loss='mse')
 
-    discriminator_1.trainable = False
-    discriminator_2.trainable = False
-    discriminator_3.trainable = False
-
+    # plot_model(discriminator_model)
     latent_space_input = Input(shape=(latent_space_dim,))
-    generator_1 = Dense(8)(latent_space_input)
-    generator_1 = LeakyReLU()(generator_1)
-    generator_1 = Dense(8, loss="sigmoid")(generator_1)
-    generator_2 = Dense(16)(generator_1)
-    generator_2 = LeakyReLU()(generator_2)
-    generator_2 = Dense(16)(generator_2)
-    generator_2 = LeakyReLU()(generator_2)
-    generator_3 = Dense(256)(generator_2)
-    generator_3 = LeakyReLU()(generator_3)
-    generator_3 = Dense(256)(generator_3)
-    generator_3 = LeakyReLU()(generator_3)
-    generator_3 = Dense(512)(generator_3)
-    generator_3 = LeakyReLU()(generator_3)
-    generator_3 = Dense(784)(generator_3)
-    generator_3 = LeakyReLU()(generator_3)
+    gen_1  = Dense(2)
+    gen_2  = Dense(16)
+    gen_3  = Dense(8)
+    gen_4  = Dense(16)
+    gen_5  = Dense(32)
+    gen_6  = Dense(32)
+    gen_7  = Dense(64)
+    gen_8  = Dense(128)
+    gen_9  = Dense(256)
+    gen_10  = Dense(512)
+    gen_11  = Dense(784)
 
-    generator_1_model = Model(latent_space_input, generator_1)
-    generator_2_model = Model(generator_1_model, generator_2)
-    generator_3_model = Model(generator_2_model, generator_3)
-    generator_1_model.compile(optimizer='adam', loss='mse')
-    generator_2_model.compile(optimizer='adam', loss='mse')
-    generator_3_model.compile(optimizer='adam', loss='mse')
+    gen_1_output = gen_4(gen_3(gen_2(gen_1(latent_space_input))))
+    gen_2_output = gen_7(gen_6(gen_5(gen_4(gen_3(gen_2(gen_1(latent_space_input)))))))
+    gen_3_output = gen_11(gen_10(gen_9(gen_8(gen_7(gen_6(gen_5(gen_4(gen_3(gen_2(gen_1(latent_space_input)))))))))))
+    gen_model = Model(latent_space_input, [gen_1_output, gen_2_output, gen_3_output] )
+    gen_model.compile(optimizer='adam', loss='mse')
 
-    output = discriminator_3_model(generator_1_model)
+    disc_1.trainable = False
+    disc_2.trainable = False
+    disc_3.trainable = False
+    disc_4.trainable = False
+    disc_5.trainable = False
+    disc_6.trainable = False
 
-    generator_and_discriminator_1 = Model(latent_space_input, output)
-    generator_and_discriminator_1.compile(optimizer='adam', loss='mse')
+    print(gen_1_output.shape)
+    print(gen_2_output.shape)
+    print(gen_3_output.shape)
 
-    gen_2_disc_2 = Model(generator_2_model, discriminator_2_model)
-    disc_2_disc_3 = discriminator_3_model(gen_2_disc_2)
-    generator_and_discriminator_2 = Model(latent_space_input, disc_2_disc_3)
-    generator_and_discriminator_2.compile(optimizer='adam', loss='mse')
+    gen_disc_output = \
+        disc_6(disc_5(Concatenate()([gen_1_output,
+            disc_4(Concatenate()([gen_2_output,
+                disc_3(disc_2(disc_1(gen_3_output)))
+                                ]))])))
 
-    gen_3_disc_1 = Model(generator_3_model, discriminator_1_model)
-    disc_1_disc_2 = discriminator_2_model(gen_3_disc_1)
-    disc_2_disc_3_bis = discriminator_3_model(disc_1_disc_2)
-    generator_and_discriminator_3 = Model(latent_space_input, disc_2_disc_3_bis)
-    generator_and_discriminator_3.compile(optimizer='adam', loss='mse')
+    gen_disc = Model(latent_space_input, gen_disc_output)
+    gen_disc.compile(optimizer='adam', loss='mse')
 
-    return generator_and_discriminator_1, generator_1_model, discriminator_1_model, generator_and_discriminator_2, generator_2_model, discriminator_2_model, generator_and_discriminator_3, generator_3_model, discriminator_3_model
+    return gen_disc, gen_model, discriminator_model
 
-def my_msggan(data, data_2, data_3, n_iterations_on_disc=2, iterations_max=1000000, latent_space_dim=32, batch_size=256, save_res=False, save_iter=1000):
+def my_msg_gan(data, data_2, data_3, n_iterations_on_disc=2, iterations_max=1000000, latent_space_dim=32, batch_size=256, save_res=False, save_iter=1000):
     seconds = int(time.time())
 
     nb_data = data.shape[0]
@@ -122,7 +147,7 @@ def my_msggan(data, data_2, data_3, n_iterations_on_disc=2, iterations_max=10000
       write_grads=True
     )
 
-    generator_and_discriminator, generator_model, discriminator_model = create_msg_model(data_shape_1, data_shape_2, data_shape_3, latent_space_dim)
+    generator_and_discriminator_1, generator_1_model, discriminator_1_model, generator_and_discriminator_2, generator_2_model, discriminator_2_model, generator_and_discriminator_3, generator_3_model, discriminator_3_model = create_msg_model(data_shape_1, data_shape_2, data_shape_3, latent_space_dim)
     discriminator_tensorboard.set_model(discriminator_model)
     gen_disc_tensorboard.set_model(generator_and_discriminator)
 
@@ -182,14 +207,20 @@ def my_msggan(data, data_2, data_3, n_iterations_on_disc=2, iterations_max=10000
     generate_grid(generator_model, 10, latent_space_dim)
     return generator_model, discriminator_model, generator_and_discriminator
 
+x_train_32 = np.resize(x_train, (x_train.shape[0], 32))
+x_train_64 = np.resize(x_train, (x_train.shape[0], 64))
 
-# generator, discriminator, generator_and_discriminator = my_gan(x_train,
-#                                                                latent_space_dim=2,
-#                                                                n_iterations_on_disc=5,
-#                                                                iterations_max=100000,
-#                                                                batch_size=512,
-#                                                                save_res=True,
-#                                                                save_iter=1000)
+
+
+generator, discriminator, generator_and_discriminator = my_msg_gan(x_train,
+                                                               x_train_64, 
+                                                               x_train_32,
+                                                               latent_space_dim=2,
+                                                               n_iterations_on_disc=5,
+                                                               iterations_max=100000,
+                                                               batch_size=512,
+                                                               save_res=True,
+                                                               save_iter=1000)
 
 # generate_grid(generator, 10, 2)
-# load_and_show(gan_folder="gan_1558610494")
+# load_and_show(gan_folder="gan_1558610494"))
